f6f466eae65a434f28e497c2a9b7266623131943
Benams <namatitiben@gmail.com>
2022-01-31 12:03:51 +0300

    Set up unit test

--- index.test.js
+++ index.test.js
@@ -6,6 +6,11 @@ describe('robotWarehouse', () => {
             const validCommands = ['N', 'S', 'E', 'W'];
             const response = app.validateCommands(validCommands);
             expect(response).toBe(true);
-        })
+        }),
+            it('should return false in case we have an invalid commands', () => {
+                const validCommands = ['N', 'S', 'EW', 'W'];
+                const response = app.validateCommands(validCommands);
+                expect(response).toBe(false);
+            })
     });
 });
