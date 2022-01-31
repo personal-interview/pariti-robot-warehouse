6c481af6da65432f003ce3bfb9476398c1d472e0
Benams <namatitiben@gmail.com>
2022-01-31 10:44:38 +0300

    Command validation

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-let grid = Array(10)
+let grid = new Array(10);
 function formualteGrid() {
+
+    for (i = 0; i < 10; i++) {
+        grid[i] = new Array(10);
+    }
+
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
-            console.log(i + " " + j);
+            // console.log(i + " " + j);
+            grid[i][j] = 'X';
         }
     }
+    console.log(grid);
 }
 
-formualteGrid();
+const validCommands = ['N', 'S', 'E', 'W', ' '];
+function validateCommands(commandString = "N S Y E") {
+    let commandArray = commandString.split(" ");
+    let validity = false;
+    for (i = 0; i < commandArray.length; i++) {
+        validity = validCommands.includes(commandArray[i]);
+        if (!validity) {
+            break;
+        }
+    }
+    return validity;
+}
+
+const initialRobotPosition = [5, 5];
+
+validateCommands();
