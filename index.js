ca584ccc35b1555d0e4226e8a0345a4d1def9439
Benams <namatitiben@gmail.com>
2022-01-31 10:22:05 +0300

    Initial set up

---
+++ index.js
@@ -0,0 +1,10 @@
+let grid = Array(10)
+function formualteGrid() {
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            console.log(i + " " + j);
+        }
+    }
+}
+
+formualteGrid();
